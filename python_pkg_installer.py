2555cd8e6d40cef7a07f92e806fc7bfd1e03b4f3
Partha Ghosh <38184323+codeunik@users.noreply.github.com>
2023-02-12 12:34:40 +0100

    Update python_pkg_installer.py

--- python_pkg_installer.py
+++ python_pkg_installer.py
@@ -3,3 +3,4 @@ import os
 os.system("conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch")
 os.system("conda install -c anaconda pandas numpy scipy jupyter")
 os.system("conda install -c conda-forge matplotlib")
+os.system("pip install cppyy")
