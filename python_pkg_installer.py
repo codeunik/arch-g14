d13e91a2126ce3e11cd7c384457794e0cbcadc6e
Partha Ghosh <38184323+codeunik@users.noreply.github.com>
2023-02-12 12:45:36 +0100

    Update python_pkg_installer.py

--- python_pkg_installer.py
+++ python_pkg_installer.py
@@ -1,6 +1,6 @@
 import os
 
 os.system("conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch")
-os.system("conda install -c anaconda pandas numpy scipy jupyter")
+os.system("conda install -c anaconda numpy pandas scipy jupyter")
 os.system("conda install -c conda-forge matplotlib")
-os.system("pip install whisper copilot cppyy")
+os.system("pip install transformers tqdm selenium opencv-python whisper copilot deep-translator cppyy")
