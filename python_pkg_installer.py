a380db1420d3a943ff3c308ac7620926720c1a9b
Partha Ghosh <38184323+codeunik@users.noreply.github.com>
2023-02-20 19:22:45 +0100

    Update python_pkg_installer.py

--- python_pkg_installer.py
+++ python_pkg_installer.py
@@ -1,6 +1,5 @@
 import os
 
 os.system("conda install -y pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia")
-os.system("conda install -y -c anaconda numpy pandas scipy jupyter")
 os.system("conda install -y -c conda-forge matplotlib cppyy pypy cython")
-os.system("pip install transformers nltk tqdm selenium opencv-python pyperclip openai-whisper copilot deep-translator")
+os.system("pip install numpy pandas scipy jupyter transformers nltk tqdm selenium opencv-python pyperclip openai-whisper copilot deep-translator")
