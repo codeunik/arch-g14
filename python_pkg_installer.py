3954947c8efb47391f8c196a374636912018e7f1
Partha Ghosh <38184323+codeunik@users.noreply.github.com>
2023-02-21 21:49:41 +0100

    Update python_pkg_installer.py

--- python_pkg_installer.py
+++ python_pkg_installer.py
@@ -1,6 +1,6 @@
 import os
 
-os.system("pip install numpy jupyter transformers nltk tqdm selenium pyperclip openai-whisper copilot deep-translator")
+os.system("pip install numpy jupyter transformers nltk tqdm selenium pyperclip openai-whisper terminal-copilot deep-translator")
 os.system("conda install -y pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia")
 os.system("conda install -y -c conda-forge matplotlib pandas opencv scipy cython")
 # cppyy, pypy
